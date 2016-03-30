7233bc47ba8ec164f8e6559cfd780e7e7c1633b5
claire <c.sus.tolan@gmail.com>
2016-03-30 19:51:10 +0200

    frozen side menu on  ddesktop chapter view. closes the-engine-room/responsible-data-handbook#12

--- assets/js/application.js
+++ assets/js/application.js
@@ -25,4 +25,25 @@ $(document).ready(function(){
 		return false;
 	});
 
+
+
+
+});
+
+$(window).bind('scroll', function () {
+	if($(window).width()>850){
+	    if ($(window).scrollTop() > 50)
+	    {
+	        $('#sidemenu').addClass('fixed');
+	    } else {
+	        $('#sidemenu').removeClass('fixed');
+	    }
+
+	    if ((window.innerHeight + window.scrollY + 500) >= document.body.offsetHeight){
+	    	$('#sidemenu').removeClass('fixed');
+	    	$('#sidemenu').addClass('gone');
+	    }else{
+	    	$('#sidemenu').removeClass('gone');
+	    }
+	}
 });
