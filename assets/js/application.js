673b867328ca6ffe7c2e637c0861039416247910
claire <c.sus.tolan@gmail.com>
2016-03-30 21:21:10 +0200

    Grid on front page. Closes the-engine-room/responsible-data-handbook#21

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,24 +1,7 @@
 $(document).ready(function(){
 	$('.post p strong').before("**").after("**");
 	
-	var $grid = $('.grid').isotope({
-	  itemSelector: '.grid-item',
-	  layoutMode: 'fitRows'
-	});
-
-	$grid.isotope({filter: '*'});
-
-
-	$('.filter-group').on( 'click', 'li', function() {
-	  $('.filter-group li').removeClass('active');
-	  $(this).addClass('active')
-	  var filterValue = $(this).attr('data-filter');
-	  console.log(filterValue);
-	  $grid.isotope({ filter: filterValue }).css('overflow','hidden');
-	});
-
-
-
+	
 	$('#menuswitch').click(function(e) {
 		$(this).toggleClass('on');
 		$('#menu').toggleClass('mobile');
